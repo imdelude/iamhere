5d7217e91a00b4cc41a09847a1ef4376760609e9
Joakim Skoog <glasvegas@hotmail.se>
2015-11-25 14:24:19 +0100

    Metainfo now implements IMetaData

--- iamhere/Responses/Geocoding/Raw/Metainfo.cs
+++ iamhere/Responses/Geocoding/Raw/Metainfo.cs
@@ -1,11 +1,15 @@
 using System;
+using iamhere.Responses.Geocoding.Contract;
 
 namespace iamhere.Responses.Geocoding.Raw
 {
-    public class Metainfo
+    public class MetaInfo : IMetadata
     {
-        public DateTime Timestamp { get; set; }
+        public DateTime TimeStamp { get; set; }
         public int? NextPageInformation { get; set; }
         public int? PreviousPageInformation { get; set; }
+
+        public int? PreviousPage => PreviousPageInformation;
+        public int? NextPage => NextPageInformation;
     }
 }
